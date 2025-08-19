4e821b26ec35149a4a3b9eac4b3714618c47d61c
Iago Yuri Rossan <iagorossan321@gmail.com>
2025-08-18 22:04:17 -0300

    Estrutura inicial form Parceiro
    Criado estrutura incial do formulario do parceiro
    falta implementar: Sombra , ajustar tamanho das colunas, e estilos conforme o figma.
    
    Co-Authored-By: Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PartnerForm from './components/formPartner';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <PartnerForm />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
