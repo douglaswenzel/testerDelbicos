b7dc9828560528810f536b1e80e9e30ebbe77b5e
douglaswenzel <douglas.wenzel@hotmail.com>
2025-08-20 12:59:34 -0300

    Remove PartnerForm and StatusBar from App component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import PartnerForm from './components/formPartner';
 import Configuration from './screens/configuration';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PartnerForm />
-      <Text>Open up App.tsx to start working on your app!</Text>
       <Configuration /> 
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -19,6 +15,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    width: 1280,
+    margin: 0,
     justifyContent: 'center',
   },
 });
